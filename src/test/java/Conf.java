d5bc5813f2e890621118091a6c2f8934257f81a0
Gary <grather@gmail.com>
2020-05-26 08:12:40 -0700

    add conf class

---
+++ src/test/java/Conf.java
@@ -0,0 +1,32 @@
+
+
+public class Conf {
+	//String myConnectionURL = "jdbc:oracle:thin:@172.19.129.63:1521:DDCD";
+	//String user = "dtsdm";
+	//String password = "Or#cl3cL3ar#12";
+	
+	String myConnectionURL = "jdbc:oracle:thin:@10.1.10.201:1521:ORCLPDB";
+	String user = "dtsdm";
+	String password = "cL3ar#12";
+	
+	public String getMyConnectionURL() {
+		return myConnectionURL;
+	}
+	public void setMyConnectionURL(String myConnectionURL) {
+		this.myConnectionURL = myConnectionURL;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
