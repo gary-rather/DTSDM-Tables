8bb70ebea7e4b422d9c44663b79122a7be7de815
KevinSapp7 <65554551+KevinSapp7@users.noreply.github.com>
2020-06-11 12:11:49 -0400

    Add files via upload
    ReasonRfrncWhTest Check-In

---
+++ src/test/java/ReasonRfrncWhTest.java
@@ -0,0 +1,289 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReasonRfrncWhTest extends TableTest{
+
+    @BeforeClass
+    public static void openResults() {
+        wr = new WriteResults("ReasonRfrncWhTest.html");
+        wr.pageHeader();
+    }
+
+    @Test
+    public void test01() {
+
+        //check that the unknown record 0 is populated
+        //EXPECT: Unspecified data row: Initial load must add the 'Unspecified' row to the table.
+
+        // Log the Class and method
+        System.out.println("Starting " + this.getClass().getSimpleName() + " " + new Throwable().getStackTrace()[0].getMethodName());
+        wr.printDiv(this.getClass().getSimpleName() + " " + new Throwable().getStackTrace()[0].getMethodName());
+
+        String sql = "SELECT * FROM DTSDM.REASON_RFRNC_WH WHERE REASON_WID = 0";
+
+        // log the Sql
+        ArrayList<SqlObject> theSql = new ArrayList<SqlObject>();
+        SqlObject sqlObj = new SqlObject("sql", sql.replaceAll("\n", "\n<br>"));
+        theSql.add(sqlObj);
+        wr.logSql(theSql);
+
+        int number = 0;
+
+        System.out.println("Starting ReasonRfrncWhTest.test1,sql");
+        try {
+            try (PreparedStatement ps = this.conn.prepareStatement(sql)) {
+                // ps.setInt(1, userId);
+                try (ResultSet rs = ps.executeQuery();) {
+                    // System.out.println("Size of results = " + rs.getInt(1));
+                    while (rs.next()) {
+                        number++;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Log the results before
+        ArrayList<ResultObject> roList = new ArrayList<ResultObject>();
+        ResultObject ro = new ResultObject((number == 1), "(number == 1)");
+        roList.add(ro);
+        wr.logTestResults(roList);
+
+        System.out.println("Test ReasonRfrncWh Row 0 1 = " + number);
+        assertEquals(1, number);
+
+        System.out.println("Finish ReasonRfrncWhTest.test1");
+
+    }
+
+    @Test
+    public void test02() {
+
+        //Check the population of the unique identifier (ORG_ACCNT_WH.ORG_ACCNT_WID (PK) column)
+
+        // Log the Class and method
+        System.out.println("Starting " + this.getClass().getSimpleName() + " " + new Throwable().getStackTrace()[0].getMethodName());
+        wr.printDiv(this.getClass().getSimpleName() + " " + new Throwable().getStackTrace()[0].getMethodName());
+
+        String sql1 = "SELECT REASON_WID FROM DTSDM.REASON_RFRNC_WH " +
+                        "GROUP BY REASON_WID HAVING COUNT(*) > 1";
+
+        String sql2 = "select count (distinct REASON_RFRNC_WH.REASON_WID) from DTSDM.REASON_RFRNC_WH";
+        String sql3 = "select count(*) from DTSDM.REASON_RFRNC_WH";
+
+        // log the Sql
+        ArrayList<SqlObject> theSql = new ArrayList<SqlObject>();
+
+        SqlObject sql1Obj = new SqlObject("sql1", sql1.replaceAll("\n", "\n<br>"));
+        theSql.add(sql1Obj);
+
+        SqlObject sql2Obj = new SqlObject("sql2", sql2.replaceAll("\n", "\n<br>"));
+        theSql.add(sql2Obj);
+
+        SqlObject sql3Obj = new SqlObject("sql3", sql3.replaceAll("\n", "\n<br>"));
+        theSql.add(sql3Obj);
+
+        wr.logSql(theSql);
+
+        int count = 0;
+        int distinctCount = 0;
+        int duplicateCount = 0;
+
+        System.out.println("Starting ReasonRfrncWhTest.test2,sql1");
+        try {
+            try (PreparedStatement ps = this.conn.prepareStatement(sql1)) {
+                // ps.setInt(1, userId);
+                try (ResultSet rs = ps.executeQuery();) {
+                    // System.out.println("Size of results = " + rs.getInt(1));
+                    while (rs.next()) {
+                        duplicateCount = rs.getInt("count(*)");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("ReasonRfrncWh.test2 sql1 failed");
+            e.printStackTrace();
+        }
+
+        System.out.println("Starting ReasonRfrncWhTest.test2,sql2");
+        try {
+            try (PreparedStatement ps = this.conn.prepareStatement(sql2)) {
+                // ps.setInt(1, userId);
+                try (ResultSet rs = ps.executeQuery()) {
+                    // System.out.println("Size of results = " + rs.getInt(1));
+                    while (rs.next()) {
+                        distinctCount = rs.getInt(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("ReasonRfrncWh.test2 sql2 failed");
+            e.printStackTrace();
+        }
+
+        System.out.println("Starting ReasonRfrncWhTest.test2,sql3");
+        try {
+            try (PreparedStatement ps = this.conn.prepareStatement(sql3)) {
+                // ps.setInt(1, userId);
+                try (ResultSet rs = ps.executeQuery();) {
+                    // System.out.println("Size of results = " + rs.getInt(1));
+                    while (rs.next()) {
+                        count = rs.getInt(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("ReasonRfrncWh.test2 sql3 failed");
+            e.printStackTrace();
+        }
+
+        // Log the results before
+        ArrayList<ResultObject> roList = new ArrayList<ResultObject>();
+
+        ResultObject ro1 = new ResultObject((distinctCount == count), "(distinctCount == count)");
+        roList.add(ro1);
+
+        ResultObject ro2 = new ResultObject((duplicateCount == 0), "(duplicateCount == 0)");
+        roList.add(ro2);
+
+        wr.logTestResults(roList);
+
+        System.out.println("Test 2: Count = " + count);
+        System.out.println("Test 2: Distinct Count = " + distinctCount);
+        System.out.println("Test 2: Duplicate Count =  " + duplicateCount);
+
+        assertEquals(count, distinctCount);
+        assertEquals(0, duplicateCount);
+
+        System.out.println("Finish ReasonRfrncWhTest.test2");
+        System.out.println();
+
+    }
+
+    @Test
+    public void test03() {
+
+        // Check the population of the REASON_RFRNC_WH.REASON_CD column
+
+        // Log the Class and method
+        System.out.println("Starting " + this.getClass().getSimpleName() + " " + new Throwable().getStackTrace()[0].getMethodName());
+        wr.printDiv(this.getClass().getSimpleName() + " " + new Throwable().getStackTrace()[0].getMethodName());
+
+        String sql = "select count(*) from" +
+                        "( \n" +
+                        "SELECT A.REASON_CD, B.U##CODE \n" +
+                        "FROM DTSDM.REASON_RFRNC_WH A, FRED.REASONCODE B \n" +
+                        "WHERE A.REASON_CD != B.U##CODE \n" +
+                        "AND A.REASON_DESCR = B.REASON_DESC \n" +
+                        ")";
+
+        // log the Sql
+        ArrayList<SqlObject> theSql = new ArrayList<SqlObject>();
+
+        SqlObject sqlObj = new SqlObject("sql", sql.replaceAll("\n", "\n<br>"));
+        theSql.add(sqlObj);
+
+        wr.logSql(theSql);
+
+        int testCount = 0;
+
+        System.out.println("Starting ReasonRfrncWhTest.test03,sql");
+        try {
+            try (PreparedStatement ps = this.conn.prepareStatement(sql)) {
+                // ps.setInt(1, userId);
+                try (ResultSet rs = ps.executeQuery();) {
+                    // System.out.println("Size of results = " + rs.getInt(1));
+                    while (rs.next()) {
+                        testCount = rs.getInt(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("ReasonRfrncWh.test03 sql failed");
+            e.printStackTrace();
+        }
+
+        ArrayList<ResultObject> roList = new ArrayList<ResultObject>();
+
+        ResultObject ro1 = new ResultObject((testCount == 0), "(testCount == 0)");
+        roList.add(ro1);
+
+        wr.logTestResults(roList);
+
+        System.out.println("Count for this test should be 0. This will confirm proper population of this column.");
+        System.out.println("Test 03: Test Count = " + testCount);
+        assertEquals(0, testCount);
+
+        System.out.println("Finish ReasonRfrncWhTest.test03");
+        System.out.println();
+
+    }
+
+    @Test
+    public void test04() {
+
+        // Check the population of the REASON_RFRNC_WH.REASON_DESCR column
+
+        // Log the Class and method
+        System.out.println("Starting " + this.getClass().getSimpleName() + " " + new Throwable().getStackTrace()[0].getMethodName());
+        wr.printDiv(this.getClass().getSimpleName() + " " + new Throwable().getStackTrace()[0].getMethodName());
+
+        String sql = "select count(*) from" +
+                        "( \n" +
+                        "SELECT DISTINCT A.REASON_DESCR, B.REASON_DESC \n" +
+                        "FROM DTSDM.REASON_RFRNC_WH A, FRED.REASONCODE B \n" +
+                        "WHERE A.REASON_CD = B.U##CODE \n" +
+                        "AND A.REASON_DESCR != B.REASON_DESC \n" +
+                        ")";
+
+        // log the Sql
+        ArrayList<SqlObject> theSql = new ArrayList<SqlObject>();
+
+        SqlObject sqlObj = new SqlObject("sql", sql.replaceAll("\n", "\n<br>"));
+        theSql.add(sqlObj);
+
+        wr.logSql(theSql);
+
+        int testCount = 0;
+
+        System.out.println("Starting ReasonRfrncWhTest.test04,sql");
+        try {
+            try (PreparedStatement ps = this.conn.prepareStatement(sql)) {
+                // ps.setInt(1, userId);
+                try (ResultSet rs = ps.executeQuery();) {
+                    // System.out.println("Size of results = " + rs.getInt(1));
+                    while (rs.next()) {
+                        testCount = rs.getInt(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("ReasonRfrncWh.test04 sql failed");
+            e.printStackTrace();
+        }
+
+        ArrayList<ResultObject> roList = new ArrayList<ResultObject>();
+
+        ResultObject ro1 = new ResultObject((testCount == 0), "(testCount == 0)");
+        roList.add(ro1);
+
+        wr.logTestResults(roList);
+
+        System.out.println("Count for this test should be 0. This will confirm proper population of this column.");
+        System.out.println("Test 04: Test Count = " + testCount);
+        assertEquals(0, testCount);
+
+        System.out.println("Finish ReasonRfrncWhTest.test04");
+        System.out.println();
+
+    }
+
+}
